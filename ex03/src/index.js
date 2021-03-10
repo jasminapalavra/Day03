ec94be599c9589ea39f4f2daa23d1561f75d53a1
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-10 17:47:27 +0100

    Added ex03 src and index.js

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+// main is a JS function
+function main() {
+  // Only change code bellow this line
+  var forCat;
+  // Only above code bellow this line
+
+}
+
+/*
+  main function will do nothing at this point
+*/
+console.log(main());
+
+module.exports = main;
