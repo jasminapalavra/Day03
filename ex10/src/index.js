af11b733eb41182d662ef986a2c13011864223de
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-10 22:50:44 +0100

    Added ex10 src and index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,25 @@
+function main(numOne, numTwo) {
+
+  // Only change code bellow this line
+
+  var numOne = numOne;
+
+  var numTwo = numTwo;
+
+  var product = numOne * numTwo;
+
+  // Only above code bellow this line
+
+  return product;
+
+}
+
+
+
+console.log(main(15, 3));
+
+console.log(main(25, 8));
+
+console.log(main(425, 10));
+
+module.exports = main;
