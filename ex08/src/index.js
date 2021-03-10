bc3eb84e072f228625271573955de0860615dd7d
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-10 22:41:48 +0100

    Added ex08 src and index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,24 @@
+function main(numOne, numTwo) {
+
+  // Only change code bellow this line
+
+  var numOne = numOne;
+
+  var numTwo = numTwo;
+
+  var sum = numOne + numTwo;
+
+  // Only above code bellow this line
+
+  return sum;
+
+}
+
+
+console.log(main(15, 8));
+
+console.log(main(5, 28));
+
+console.log(main(150, 425));
+
+module.exports = main;
