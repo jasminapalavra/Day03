dfb279474463e2cb2618f70f03a08ad540663c3b
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-10 22:35:53 +0100

    Added ex07 src and index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,43 @@
+function main() {
+
+  // Only change code bellow this line
+
+  // Variable declarations
+
+  var studlyCapVar;
+
+  var properCamelCase;
+
+  var titleCaseOver;
+
+
+
+  // Variable assignments
+
+  studlyCapVar = 10;
+
+  properCamelCase = "A String";
+
+  titleCaseOver = 9000;
+
+ 
+
+  // Only above code bellow this line
+
+  return {
+
+    studlyCapVar,
+
+    properCamelCase,
+
+    titleCaseOver
+
+  }
+
+}
+
+
+
+console.log(main());
+
+module.exports = main;
