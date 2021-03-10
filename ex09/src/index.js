7e17d47fe44ec2b82e8f4d56b98ab8c397ff7625
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-10 22:45:50 +0100

    Added ex09 src and index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,25 @@
+function main(numOne, numTwo) {
+
+  // Only change code bellow this line
+
+  var numOne = numOne;
+
+  var numTwo = numTwo;
+
+  var difference = numOne - numTwo;
+
+  // Only above code bellow this line
+
+  return difference;
+
+}
+
+
+
+console.log(main(15, 8));
+
+console.log(main(25, 8));
+
+console.log(main(425, 120));
+
+module.exports = main;
