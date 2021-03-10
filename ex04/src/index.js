ee491d0fe2183e60e06ca74ce2a572027ac770af
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-10 17:55:28 +0100

    Added ex04 src and index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+// main is a JS function
+function main() {
+  // Only change code bellow this line
+  var x = 10;
+  var y = x;
+  // Only above code bellow this line
+  return [x, y];
+
+}
+
+/*
+  main function will do nothing at this point
+*/
+console.log(main());
+
+module.exports = main;
